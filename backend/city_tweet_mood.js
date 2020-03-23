5f6aa6b2b9b56b11ad827fc009723d6dd30b6d1d
rooneyshuman <rooneyshuman@gmail.com>
2020-03-23 16:22:27 -0700

    Add form element for coordinates

--- backend/city_tweet_mood.js
+++ backend/city_tweet_mood.js
@@ -11,7 +11,7 @@ const twitter_client = new Twitter({
   consumer_key: twitter_secrets.TWITTER_API,
   consumer_secret: twitter_secrets.TWITTER_SECRET,
   access_token_key: twitter_secrets.TWITTER_ACCESS_TOKEN,
-  access_token_secret: twitter_secrets.TWITTER_ACCESS_SECRET,
+  access_token_secret: twitter_secrets.TWITTER_ACCESS_SECRET
 });
 const cities = require("../cities");
 
@@ -22,7 +22,7 @@ const cities = require("../cities");
  */
 function get_city_coordinates(city_name, state_name) {
   let city = cities.find(
-    (data) => data.city.match(city_name) && data.state.match(state_name)
+    data => data.city.match(city_name) && data.state.match(state_name)
   );
   return city.latitude.toString() + "," + city.longitude.toString();
 }
@@ -42,15 +42,15 @@ async function get_city_tweets(city_name, state_name, num_of_tweets) {
     geocode: city_coordinate_info + ",2mi",
     lang: "en",
     count: num_of_tweets,
-    tweet_mode: "extended",
+    tweet_mode: "extended"
   };
   let tweets = await twitter_client.get("search/tweets", search_params);
-  return Promise.resolve(tweets.statuses.map((tweet) => tweet.full_text));
+  return Promise.resolve(tweets.statuses.map(tweet => tweet.full_text));
 }
 
 async function sentiment_helper(doc) {
   let [result] = await google_client.analyzeSentiment({
-    document: doc,
+    document: doc
   });
   return Promise.resolve(result.documentSentiment);
 }
@@ -63,13 +63,13 @@ async function sentiment_helper(doc) {
  */
 function get_sentiment(text_arr) {
   return Promise.all(
-    text_arr.map(async (text) => {
+    text_arr.map(async text => {
       return {
         text: text,
         sentiment: await sentiment_helper({
           content: text,
-          type: "PLAIN_TEXT",
-        }),
+          type: "PLAIN_TEXT"
+        })
       };
     })
   );
@@ -91,5 +91,5 @@ async function get_tweets_and_sentiment(city_name, state_name) {
 
 module.exports = {
   get_tweets_and_sentiment: get_tweets_and_sentiment,
-  _get_city_coordinates: get_city_coordinates,
+  _get_city_coordinates: get_city_coordinates
 };
