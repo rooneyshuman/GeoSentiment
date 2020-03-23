29f698d20dae4b9c5b54e5d77d12a429011c8c9d
rooneyshuman <rooneyshuman@gmail.com>
2020-03-22 22:47:28 -0700

    Retrieve coords based on city name

--- city_tweet_mood.js
+++ city_tweet_mood.js
@@ -1,28 +1,39 @@
-'use strict';
-const path = require('path');
-const process = require('process');
-process.env.GOOGLE_APPLICATION_CREDENTIALS = process.cwd() + path.sep + 'google_sa.json';
-const twitter_secrets = require('./twitter_keys');
-const language = require('@google-cloud/language');
-const Twitter = require('twitter');
+"use strict";
+const path = require("path");
+const process = require("process");
+process.env.GOOGLE_APPLICATION_CREDENTIALS =
+  process.cwd() + path.sep + "google_sa.json";
+const twitter_secrets = require("./twitter_keys");
+const language = require("@google-cloud/language");
+const Twitter = require("twitter");
 const google_client = new language.LanguageServiceClient();
 const twitter_client = new Twitter({
-    consumer_key: twitter_secrets.TWITTER_API,
-    consumer_secret: twitter_secrets.TWITTER_SECRET,
-    access_token_key: twitter_secrets.TWITTER_ACCESS_TOKEN,
-    access_token_secret: twitter_secrets.TWITTER_ACCESS_SECRET
+  consumer_key: twitter_secrets.TWITTER_API,
+  consumer_secret: twitter_secrets.TWITTER_SECRET,
+  access_token_key: twitter_secrets.TWITTER_ACCESS_TOKEN,
+  access_token_secret: twitter_secrets.TWITTER_ACCESS_SECRET
 });
-
+const axios = require("axios");
+const maps_client = require("@googlemaps/google-maps-services-js").Client;
+const google_secrets = require("./google_sa");
 
 /**
- * TODO - make a city -> coordinate database
  * @param city_name
  * @returns string of gps coordinates (lat,long)
  */
-function get_city_coordinates(city_name) {
-    // PLACEHOLDER FOR GETTING THE CITY COORDINATES VIA API
-    return '45.523,-122.676';
-}
+let get_city_coordinates = async city_name => {
+  const geocoder = new maps_client({});
+  let res = await geocoder.geocode({
+    params: {
+      address: city_name,
+      key: google_secrets.api_key
+    },
+    timeout: 1000 // milliseconds
+  });
+  var lat = res.data.results[0].geometry.location.lat.toString();
+  var lng = res.data.results[0].geometry.location.lng.toString();
+  return lat + "," + lng;
+};
 
 /**
  * get_city_tweets
@@ -32,24 +43,24 @@ function get_city_coordinates(city_name) {
  * @returns an array of the tweet's text
  */
 async function get_city_tweets(city_name, num_of_tweets) {
-    const city_coordinate_info = get_city_coordinates(city_name);
-    const search_params = {
-        q: '',
-        geocode: city_coordinate_info + ',2mi',
-        lang: 'en',
-        count: num_of_tweets,
-        tweet_mode: 'extended'
-    };
-    let tweets = await twitter_client.get('search/tweets', search_params);
-    return Promise.resolve(tweets.statuses.map(tweet => tweet.full_text));
+  const city_coordinate_info = await get_city_coordinates(city_name);
+  const search_params = {
+    q: "",
+    geocode: city_coordinate_info + ",2mi",
+    lang: "en",
+    count: num_of_tweets,
+    tweet_mode: "extended"
+  };
+  let tweets = await twitter_client.get("search/tweets", search_params);
+  return Promise.resolve(tweets.statuses.map(tweet => tweet.full_text));
 }
-
 
 async function sentiment_helper(doc) {
-    let [result] = await google_client.analyzeSentiment({document: doc});
-    return Promise.resolve(result.documentSentiment);
+  let [result] = await google_client.analyzeSentiment({
+    document: doc
+  });
+  return Promise.resolve(result.documentSentiment);
 }
-
 
 /**
  * get_sentiment
@@ -58,15 +69,17 @@ async function sentiment_helper(doc) {
  * @returns Object displaying the text and the sentiment dictionary { text: text, sentiment: {mag: float, score: float}}
  */
 async function get_sentiment(text_arr) {
-    return await Promise.all(text_arr.map(async text => {
-        return {
-            text: text,
-            sentiment: await sentiment_helper({
-                content: text,
-                type: 'PLAIN_TEXT',
-            })
-        }
-    }));
+  return await Promise.all(
+    text_arr.map(async text => {
+      return {
+        text: text,
+        sentiment: await sentiment_helper({
+          content: text,
+          type: "PLAIN_TEXT"
+        })
+      };
+    })
+  );
 }
 
 /**
@@ -76,17 +89,19 @@ async function get_sentiment(text_arr) {
  * @returns Array of text -> sentiment objects
  */
 async function get_tweets_and_sentiment(city_name) {
-    await get_city_tweets("portland", 50)
-        .then(async results => {
-            let res = await get_sentiment(results);
-            console.log(res); // TODO - refactor to just a return when using these actual values (not just testing)
-            return res;
-        })
-        .catch(err => {
-            console.error(err);
-        })
+  await get_city_tweets(city_name, 50)
+    .then(async results => {
+      let res = await get_sentiment(results);
+      console.log(res); // TODO - refactor to just a return when using these actual values (not just testing)
+      return res;
+    })
+    .catch(err => {
+      console.error(err);
+    });
 }
 
 // get_tweets_and_sentiment();  // TODO - UNCOMMENT TO TEST -- `$ node ./city_tweet_mood.js`
 
-module.exports = {get_tweets_and_sentiment};
+module.exports = {
+  get_tweets_and_sentiment
+};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var mood = require("../city_tweet_mood");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  mood.get_tweets_and_sentiment("Portland");
+  res.render("index", {
+    title: "Express"
+  });
 });
 
 module.exports = router;
