b7992bc812a8f9c3deddcda82b1825c552118b2c
rooneyshuman <rooneyshuman@gmail.com>
2020-03-23 11:34:26 -0700

    Call get tweets and sentiment on form post

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,14 @@ var mood = require("../city_tweet_mood");
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
-  mood.get_tweets_and_sentiment("Portland", "Oregon");
   res.render("index", {
     title: "TWITTER MOOD"
   });
 });
 
+router.post("/mood", function(req, res) {
+  var tweets = mood.get_tweets_and_sentiment(req.body.city, req.body.state);
+  console.log(`RETREIVED: ${tweets}`);
+  res.render("tweets", { title: "TWITTER MOOD", tweets: tweets });
+});
 module.exports = router;
