b029b6cb9f490a0b62ae2ab77581c063639d19de
loganballard <loganballard@gmail.com>
2020-04-09 20:26:05 -0700

    adding a test and a readme

--- test/test_tweet_mood.js
+++ test/test_tweet_mood.js
@@ -1,20 +1,24 @@
 const chai = require("chai");
 const assert = chai.assert;
-const should = chai.should();
-const city_mood = require("../backend/city_tweet_mood");
+const expect = chai.expect;
+const rewire = require("rewire");
+const city_mood = rewire("../backend/city_tweet_mood.js");
 
-// describe("Coordinates Retrieved by City/State Combo", () => {
-//   it("Should Return a String", () => {
-//     assert.equal(
-//       typeof city_mood._get_city_coordinates("Portland", "Oregon"),
-//       "string"
-//     );
-//   });
+describe("Get Sentiment w/ Valid Text", () => {
+  const get_sentiment = city_mood.__get__("get_sentiment");
+  let text_to_analyze = ["positive", "negative", "neutral"];
+  let results = get_sentiment(text_to_analyze);
 
-//   it("Should Return Correctly for Portland, OR", () => {
-//     assert.equal(
-//       city_mood._get_city_coordinates("Portland", "Oregon"),
-//       "45.5230622,-122.6764816"
-//     );
-//   });
-// });
+  it("Should have basic length and data characteristics", () => {
+    expect(results).to.be.an("array");
+    expect(results).to.be.length(3);
+    results.forEach((sentiment) => {
+      expect(sentiment).to.be.an("object");
+      expect(sentiment.content).to.be.a("string");
+      expect(sentiment.score).to.be.a("number");
+      expect(sentiment.positive).to.be.a("number");
+      expect(sentiment.negative).to.be.a("number");
+      expect(sentiment.neutral).to.be.a("number");
+    });
+  });
+});
